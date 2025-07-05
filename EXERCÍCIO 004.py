7afa7fc90a383d63bf6411fc40fcd0ece94e8907
rafael santos <rafaelsantosmota2011@gmail.com>
2025-07-05 15:00:56 -0300

    Update EXERCÍCIO 004.py

--- EXERCÍCIO 004.py
+++ EXERCÍCIO 004.py
@@ -1,102 +1,39 @@
-print('DESAFIO 004')
+print ('>>>DESAFIO 004<<<')
 
-print('°'*11)
-print()
+print('—'*70)
 
-print('-'*30)
-print()
+print('Faça um programa que leia algo pelo teclado e mostre na tela\n o seu tipo primitivo e todas as informações possíveis sobre ele.')
 
-print('Faça um programa que leia algo \npelo teclado, e mostre na tela \no seu tipo primitivo e todas \nas informações possíveis sobre \nele.')
+print('—'*70)
 
-print()
-print('-'*30)
-print()
-
-print('Olá aqui você pode digitar \num número e eu irei passar \ntodas as informações \npossíveis sobre ele.')
+n1 = input('Digite algo: ')
 
 print()
-print('-'*30)
-print()
 
-n=input('DIGITE ALGO: ')
+print('Tipo primitivo:', type(n1))
 
 print()
-print('-'*30)
 
-print('Tipo primitivo:', type(n))
+print('Tem letras?', n1.isalpha())
 
-print('-'*30)
+print('Tem dígitos?', n1.isdigit())
 
-print('Is/upper:', n.isupper())
+print('Tem números?', n1.isnumeric())
 
-print('-'*30)
+print('É decimal?', n1.isdecimal())
 
-print('Is/alnum:', n.isalnum())
+print('Tem letras ou números?', n1.isalnum())
 
-print('-'*30)
+print('Tem espaços?', n1.isspace())
 
-print('Is/alpha:', n.isalpha())
+print('Letras minúsculas?', n1.islower())
 
-print('-'*30)
+print('Letras maiúsculas?', n1.isupper())
 
-print('Is/digit:', n.isdigit())
+print('Começa maiúsculo?', n1.istitle())
 
-print('-'*30)
+print('É uma variável?', n1. isidentifier())
 
-print('Is/decimal:', n.isdecimal())
+print('É imprimível?', n1. isprintable())
 
-print('-'*30)
-
-print('Is/numeric:', n.isnumeric())
-
-print('-'*30)
-
-print('Is/identifier:', n.isidentifier())
-
-print('-'*30)
-
-print('Is/lower:', n.islower())
-
-print('-'*30)
-
-print('Is/printable:', n.isprintable())
-
-print('-'*30)
-
-print('Is/space:', n.isspace())
-
-print('-'*30)
-
-print('Is/title', n.istitle())
-
-print('-'*30)
-print()
-
-print('Para finalizar mostrarei o que \nsão tipos primitivo.')
-
-print()
-print('-'*30)
-print()
-
-print('Tipo INT: \n(7) (-4) (0) (9875)')
-
-print()
-print('-'*30)
-print()
-
-print('Tipo FLOAT: \n(4.5) (0.076) (-15.223) (7.0)')
-
-print()
-print('-'*30)
-print()
-
-print('Tipo BOOL: \n(True) (False)')
-
-print()
-print('-'*30)
-print()
-
-print('Tipo STR: \n("Olá") ("7.5") ("")')
-
-print()
-print('-'*30)
+print('Sem acento ou emoji?', n1.isascii())
