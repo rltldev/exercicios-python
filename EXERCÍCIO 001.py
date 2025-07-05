2c63bde3d79246553e0f6245b2a36f38c1463927
rafael santos <rafaelsantosmota2011@gmail.com>
2025-07-05 14:52:52 -0300

    CORTANDO O TEXTO GASTANDO MENOS MEMÓRIA EXERCÍCIO 001.py

--- EXERCÍCIO 001.py
+++ EXERCÍCIO 001.py
@@ -1,28 +1,13 @@
-print('DESAFIO 001')
+print ('>>>DESAFIO 001<<<')
 
-print('='*11)
-print()
+print ('—' * 70)
 
-print('•'*30)
-print()
+print ('Crie um script python que leia o nome de uma pessoa e mostre uma\n mensagem de boas-vindas de acordo com o valor digitado.')
 
-print('Crie um script python que leia, \no nome de uma pessoa e mostre \numa mensagem de boas-vindas de \nacordo com o valor digitado.')
+print ('—' * 70)
 
-print()
-print('•'*30)
-print()
+nome = input ('Qual é o seu nome ? ')
 
-print('Olá meu nome é Rafael.')
+print ()
 
-print()
-print('•'*30)
-print()
-
-nome=input('Qual é o seu nome? ')
-
-print()
-
-print('Boas-vindas {}.'.format(nome))
-
-print()
-print('•'*30)
+print ('Seja bem vindo' , 
